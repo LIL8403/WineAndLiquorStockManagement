53208b4db8225f7d98f4f9af3c232f5b342a400b
LIL8403 <153436451+LIL8403@users.noreply.github.com>
2025-02-15 14:51:57 +0100

    init tab

--- src/Dao/tables.java
+++ src/Dao/tables.java
@@ -19,11 +19,11 @@ public class tables {
         try {
             con = ConnectionProvider.getCon();
             st = con.createStatement();
-            //   st.executeUpdate("create table users(UserID int primary key AUTO_INCREMENT,UserName VARCHAR(100) NOT NULL,Email VARCHAR(100) NOT NULL UNIQUE,Password VARCHAR(100) NOT NULL,Role VARCHAR (50) NOT NULL CHECK(Role IN('Administrator','Manager','Sales staff')),Status Enum ('Active','Inactive') NOT NULL);");
-            //   st.executeUpdate("INSERT INTO users(UserName,Email,Password,Role,Status) VALUES('Lord','Lord@gmail.com','absolute','Administrator','Active');");
-            //   st.executeUpdate("create table Category(CategoryID int primary key AUTO_INCREMENT,CategoryName VARCHAR(50) NOT NULL,Description VARCHAR(100));");
-            //   st.executeUpdate("create table products(ProductID int primary key AUTO_INCREMENT,ProductName VARCHAR(50) NOT NULL,Description VARCHAR(100),Brand VARCHAR(50),Price DECIMAL(10,2) NOT NULL,Quantity INT NOT NULL DEFAULT '0',BottleSize VARCHAR(20),Category INT NOT NULL,foreign key(Category) references category(CategoryID));");
-            //st.executeUpdate("create table customer(ID int primary key AUTO_INCREMENT,Name VARCHAR(250) NOT NULL,Mobile VARCHAR(50) UNIQUE,Email VARCHAR(200) UNIQUE);");
+            st.executeUpdate("create table users(UserID int primary key AUTO_INCREMENT,UserName VARCHAR(100) NOT NULL,Email VARCHAR(100) NOT NULL UNIQUE,Password VARCHAR(100) NOT NULL,Role VARCHAR (50) NOT NULL CHECK(Role IN('Administrator','Manager','Sales staff')),Status Enum ('Active','Inactive') NOT NULL);");
+            st.executeUpdate("INSERT INTO users(UserName,Email,Password,Role,Status) VALUES('Lord','Lord@gmail.com','absolute','Administrator','Active');");
+            st.executeUpdate("create table Category(CategoryID int primary key AUTO_INCREMENT,CategoryName VARCHAR(50) NOT NULL,Description VARCHAR(100));");
+            st.executeUpdate("create table products(ProductID int primary key AUTO_INCREMENT,ProductName VARCHAR(50) NOT NULL,Description VARCHAR(100),Brand VARCHAR(50),Price DECIMAL(10,2) NOT NULL,Quantity INT NOT NULL DEFAULT '0',BottleSize VARCHAR(20),Category INT NOT NULL,foreign key(Category) references category(CategoryID));");
+            st.executeUpdate("create table customer(ID int primary key AUTO_INCREMENT,Name VARCHAR(250) NOT NULL,Mobile VARCHAR(50) UNIQUE,Email VARCHAR(200) UNIQUE);");
             st.executeUpdate("create table orderDetail(order_pk int primary key AUTO_INCREMENT,orderID VARCHAR(200) NOT NULL,customer_fk  int,orderDate VARCHAR(200),totalPaid int);");
             JOptionPane.showMessageDialog(null, "Table created successfully");
         } catch (Exception e) {
